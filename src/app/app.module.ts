84f64726e9756bd12d93be81eb269c50cf94e2f6
shahbazaamir <shahbaz07dbit@gmail.com>
2019-08-17 00:45:30 +0530

    firebase integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {Routes,RouterModule} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 import { CustDetailComponent } from './cust-detail/cust-detail.component';
 import { MCQComponent } from './mcq/mcq.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { MatButtonModule,MatInputModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material';
 import { QuizServiceService } from './quiz-service.service';
 import { HttpModule } from '@angular/http';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SubjectComponent } from './mcq/subject/subject.component';
 import { AnswerComponent } from './mcq/answer/answer.component';
 import { QuestionComponent } from './question/question.component';
-import {DemoMaterialModule} from './material-module';
+import { DemoMaterialModule } from './material-module';
 import { BackofficeComponent } from './backoffice/backoffice.component';
 import { UpdateComponent } from './backoffice/update/update.component';
- import { AngularFireModule } from '@angular/fire';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
- 
- 
 
-const appRoutes:Routes =[
-{path:'cust-detail',component: CustDetailComponent},
-{path:'mcq',component: MCQComponent},
-{path:'sign-up',component: SignUpComponent},
-{path:'app-backoffice',component: BackofficeComponent}
+
+const appRoutes: Routes = [
+  { path: 'cust-detail', component: CustDetailComponent },
+  { path: 'mcq', component: MCQComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'app-backoffice', component: BackofficeComponent }
 ];
 
 @NgModule({
@@ -41,20 +42,22 @@ const appRoutes:Routes =[
     QuestionComponent,
     BackofficeComponent,
     UpdateComponent
+
   ],
   imports: [
     BrowserModule,
-	FormsModule,
-	ReactiveFormsModule,
-	RouterModule.forRoot(appRoutes),
-	MatButtonModule,
-   MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule,
+    MatInputModule,
     MatCardModule,
-	HttpModule,
-	MatSelectModule,
-  BrowserAnimationsModule
-  ,
-  DemoMaterialModule
+    HttpModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    DemoMaterialModule
   ],
   providers: [QuizServiceService],
   bootstrap: [AppComponent]
--- src/app/backoffice/update/update.component.ts
+++ src/app/backoffice/update/update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable} from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 
@@ -8,21 +9,24 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
-users;
-  constructor( private db : AngularFirestore) {
+   
+   users: Observable<any[]>;
+  constructor(private db: AngularFirestore) {
 
-     this.users = db.list('/users');
-     console.log(this.users);
-   }
+    this.users = db.collection('test1').valueChanges();
+ 
+
+    console.log(this.users);
+  }
 
   ngOnInit() {
   }
 
-  update(){
+  update() {
     this.db.database().ref('users/' + 'userId').set({
-        username: 'name2',
-        email: 'email1',
-        profile_picture: 'imageUrl'
-      });
+      username: 'name2',
+      email: 'email1',
+      profile_picture: 'imageUrl'
+    });
   }
 }
